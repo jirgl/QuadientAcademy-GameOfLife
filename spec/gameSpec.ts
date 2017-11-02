a65bf7b4b21f68842a1121abb364be4b633a94be
jirgl <jirgl.jakub@gmail.com>
2017-11-02 22:09:02 +0100

    Add version with jasmine.clock

--- spec/gameSpec.ts
+++ spec/gameSpec.ts
@@ -1,20 +1,48 @@
 import { Game } from '../src/game';
 
-function delay(interval: number): Promise<number> {
-    return new Promise<number>(resolve => {
-        setTimeout(
-            () => { resolve(); },
-            interval);
-    });
-}
-
 describe('Game', () => {
+    let game: Game;
 
-    it('call update in correct interval', async () => {
-        const game = new Game('g1', [], 1000);
-        const updateSpy = spyOn(game, 'update');
-        await delay(2500);
-        expect(updateSpy).toHaveBeenCalledTimes(2);
+    describe('With async/await', () => {
+
+        beforeEach(() => {
+            game = new Game('g1', [], 1000);
+        });
+
+        function delay(interval: number): Promise<number> {
+            return new Promise<number>(resolve => {
+                setTimeout(
+                    () => { resolve(); },
+                    interval);
+            });
+        }
+
+        it('call update in correct interval', async () => {
+            const updateSpy = spyOn(game, 'update');
+            await delay(2500);
+            expect(updateSpy).toHaveBeenCalledTimes(2);
+        });
+
+    })
+
+    describe('With jasmine clock', () => {
+
+        beforeEach(() => {
+            jasmine.clock().install();
+            game = new Game('g1', [], 1000);
+        });
+
+        afterEach(() => {
+            jasmine.clock().uninstall();
+        });
+
+        it('call update in correct interval', () => {
+            const updateSpy = spyOn(game, 'update');
+            jasmine.clock().tick(2500);
+            expect(updateSpy).toHaveBeenCalledTimes(2);
+            jasmine.clock().uninstall();
+        });
+
     });
 
 });
